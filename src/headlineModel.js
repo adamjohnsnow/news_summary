4f749524261d16638f78e1af80525156912f61a1
Adam Snow <adamjohnsnow@icloud.com>
2017-05-26 11:58:04 +0100

    headline model

---
+++ src/headlineModel.js
@@ -0,0 +1,15 @@
+function Headlines(api) {
+
+  var contentArray = api.response.content;
+  var htmlString = '';
+  var idMap = [];
+
+  for (var i = 0; i < contentArray.length; ++i) {
+    htmlString = htmlString + '<li><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li>';
+    idMap.push(contentArray[i].id);
+  }
+    contentArray.idMap = idMap;
+    htmlString = '<ul>' + htmlString + '</ul>';
+    return document.getElementById("app").innerHTML = htmlString;
+
+}
--- testing/newsTests.js
+++ testing/newsTests.js
@@ -1,6 +1,12 @@
 (function (exports) {
   console.log('Episode 1: Basic unit tests')
 
-  
+
+  var mockApi = { "response": { "content": [{"id": 1, "webTitle": "News story one about stuff", "apiUrl": "http://news"},{"id": 2, "webTitle": "More stuff", "apiUrl": "http://news"}]}};
+  var headlines = new Headlines(mockApi);
+
+  var htmlTest = '<ul><li><a href="#0">News story one about stuff</a></li><li><a href="#1">More stuff</a></li></ul>';
+  var htmlActual = document.getElementById('app').innerHTML;
+  sameTheyAre('Generates two links from headines', htmlTest, htmlActual);
 
 })();
