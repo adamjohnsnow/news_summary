02c9bb1eebd21ce955c4cef2ded1e6cdb3c02d7b
Adam Snow <adamjohnsnow@icloud.com>
2017-05-26 14:25:54 +0100

    displays single story with picture

--- src/getNews.js
+++ src/getNews.js
@@ -1,16 +1,15 @@
-
-
 const news = "http://content.guardianapis.com/sport?show-fields=all&api-key=6cefe839-68bb-4814-8884-53cedf5c4231";
 const xhr = new XMLHttpRequest();
 const method = "GET";
 
-
 (function(exports){
+
   function getNews() {
     xhr.open(method, news, true);
     xhr.onreadystatechange = function(){
-      console.log(JSON.parse(xhr.responseText))
-        return headlines = new Headlines(JSON.parse(xhr.responseText));
+      apiResponse = JSON.parse(xhr.responseText)
+      listener = new Listeners(apiResponse)
+      return headlines = new Headlines(apiResponse);
     };
     xhr.send();
   };
--- src/headlineModel.js
+++ src/headlineModel.js
@@ -2,14 +2,11 @@ function Headlines(api) {
 
   var contentArray = api.response.results;
   var htmlString = '';
-  var idMap = [];
 
   for (var i = 0; i < contentArray.length; ++i) {
-    htmlString = htmlString + '<li><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li>';
-    idMap.push(contentArray[i].id);
+    htmlString = htmlString + '<li class="headline"><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li>';
   }
-    contentArray.idMap = idMap;
-    htmlString = '<ul>' + htmlString + '</ul>';
-    return document.getElementById("app").innerHTML = htmlString;
+  htmlString = '<ul>' + htmlString + '</ul>';
+  return document.getElementById("app").innerHTML = htmlString;
 
 }
