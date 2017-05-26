f690fe1970dc7f115117f4be97ffc1aa27781bf4
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-26 15:10:41 +0100

    adds css and animations

--- src/headlineModel.js
+++ src/headlineModel.js
@@ -5,7 +5,7 @@ function Headlines(api) {
   var idMap = [];
 
   for (var i = 0; i < contentArray.length; ++i) {
-    htmlString = htmlString + '<li class="headline"><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li><br>';
+    htmlString = htmlString + '<li class="animated bounceInLeft" id="headline"><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li><br>';
     idMap.push(contentArray[i].id);
   }
     contentArray.idMap = idMap;
