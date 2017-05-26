d7ab42c940b8048eac8eb5889ca5c1b75273c5e0
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-26 16:59:52 +0100

    restructures animation of links

--- src/headlineModel.js
+++ src/headlineModel.js
@@ -5,11 +5,11 @@ function Headlines(api) {
   var idMap = [];
 
   for (var i = 0; i < contentArray.length; ++i) {
-    htmlString = htmlString + '<li class="animated bounceInLeft" id="headline"><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li><br>';
+    htmlString = htmlString + '<li id="headline"><a href="#' + i + '">' + contentArray[i].webTitle + '</a></li><br>';
     idMap.push(contentArray[i].id);
   }
     contentArray.idMap = idMap;
-    htmlString = '<ul>' + htmlString + '</ul>';
+    htmlString = '<ul class="animated bounceInUp" style="list-style: none;>\n' + htmlString + '\n</ul>';
     return document.getElementById("app").innerHTML = htmlString;
 
 }
